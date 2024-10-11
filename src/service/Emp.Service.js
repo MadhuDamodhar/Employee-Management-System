c41bac0eed68dee0aee7a18fd484a11a12180ea8
Madhu Damodhar <madhudamodhar9@gmail.com>
2024-10-12 00:59:08 +0530

    changes commited

--- src/service/Emp.Service.js
+++ src/service/Emp.Service.js
@@ -1,27 +1,28 @@
-import axios from 'axios';
-const SERVER_URL='http://172.20.10.10:9191'
+import axios from "axios";
+const SERVER_URL =
+  "https://employee-management-system-two-beta.vercel.app/:9191";
 
-const BASE_URL=`${SERVER_URL}/api/v1/employees`;
+const BASE_URL = `${SERVER_URL}/api/v1/employees`;
 
-class Service{
-empSave(emp){
-  return axios.post(BASE_URL+"/add",emp);
+class Service {
+  empSave(emp) {
+    return axios.post(BASE_URL + "/add", emp);
+  }
+
+  getAllEmp() {
+    return axios.get(BASE_URL + "/fetch");
+  }
+
+  getEmpById(id) {
+    return axios.get(BASE_URL + "/" + id);
+  }
+
+  deleteEmpById(id) {
+    return axios.get(BASE_URL + "/delete/" + id);
+  }
+
+  updateEmp(id, emp) {
+    return axios.post(BASE_URL + "/update/" + id, emp);
+  }
 }
-
-getAllEmp(){
-  return axios.get(BASE_URL+'/fetch');
-}
-
-getEmpById(id){
-  return axios.get(BASE_URL+'/'+id);
-}
-
-deleteEmpById(id){
-  return axios.get(BASE_URL+'/delete/'+id);
-}
-
-updateEmp(id,emp){
-  return axios.post(BASE_URL+"/update/"+id,emp);
-}
-
-}export default new Service();
+export default new Service();
